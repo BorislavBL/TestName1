bac3e7dd3839a896ec48dbcafcb6793548ea4e9f
BorislavBL <borkolorinkov@gmail.com>
2024-09-04 19:26:29 +0300

    Initial Commit

---
+++ TestName1/TestName1App.swift
@@ -0,0 +1,17 @@
+//
+//  TestName1App.swift
+//  TestName1
+//
+//  Created by Borislav Lorinkov on 4.09.24.
+//
+
+import SwiftUI
+
+@main
+struct TestName1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
